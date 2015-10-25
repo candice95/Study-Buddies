2b0717e8e838a4808fe6e2d7790855ed4786aac6
arieeel1110 <arieeel1110@gmail.com>
2015-10-24 22:02:04 -0700

    Login Signup function

--- app/src/main/java/com/example/cardgame/cardgame/Application.java
+++ app/src/main/java/com/example/cardgame/cardgame/Application.java
@@ -1,6 +1,7 @@
 package com.example.cardgame.cardgame;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
 
 /**
  * Created by chenshiyu on 10/21/15.
@@ -11,6 +12,8 @@ public class Application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
 
+        Parse.enableLocalDatastore(this);
         Parse.initialize(this, Constant.PARSE_APPLICATION, Constant.PARSE_CLIENT_ID);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 }
