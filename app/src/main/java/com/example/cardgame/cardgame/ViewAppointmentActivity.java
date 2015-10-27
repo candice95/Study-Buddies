10708f377f7f97324ee8097c40f218c995b04818
shumin0809 <shumin0809@yahoo.com>
2015-10-26 21:47:58 -0700

    fixed file naming issue

--- app/src/main/java/com/example/cardgame/cardgame/ViewAppointmentActivity.java
+++ app/src/main/java/com/example/cardgame/cardgame/ViewAppointmentActivity.java
@@ -8,7 +8,7 @@ public class ViewAppointmentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_viewAppointment);
+        setContentView(R.layout.activity_view_appointment);
     }
     
 }
