264504f74086b44cb628e6d4f7bf74a6de713fe1
Zhihui Huang <zhh017@ucsd.edu>
2015-10-31 20:02:15 -0700

    ignore it

--- app/src/main/java/com/example/cardgame/cardgame/ViewAppointmentActivity.java
+++ app/src/main/java/com/example/cardgame/cardgame/ViewAppointmentActivity.java
@@ -8,11 +8,7 @@ public class ViewAppointmentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD:app/src/main/java/com/example/cardgame/cardgame/AppointmentActivity.java
-        setContentView(R.layout.activity_user_page);
-=======
-        setContentView(R.layout.activity_viewAppointment);
->>>>>>> master:app/src/main/java/com/example/cardgame/cardgame/ViewAppointmentActivity.java
+        setContentView(R.layout.activity_view_appointment);
     }
     
 }
